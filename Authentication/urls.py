477c5411cde3e92455d3b3a374bad4523c7bf9f5
dipankarkarmakar <connecttodkarmakar@gmail.com>
2018-11-12 17:19:05 +0530

    first commit

---
+++ Authentication/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import login, sample_api
+
+
+urlpatterns = [
+    path('api/login', login),
+    path('api/sampleapi', sample_api),
+]
---
+++ Authentication/views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+from rest_framework.response import Response
+# from django.contrib.auth import login, authenticate
+# from django.shortcuts import render, redirect
+# from .forms import SignUpForm
+#
+# @csrf_exempt
+# @api_view(["POST"])
+# def signup(request):
+#     form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'signup.html', {'form': form})
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'},
+                        status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key,
+                     'user_id': user.pk,
+                     'email': user.email},
+                    status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(["GET"])
+def sample_api(request):
+    data = {'sample_data': 123}
+    return Response(data, status=HTTP_200_OK)
+
+
+
+
+
